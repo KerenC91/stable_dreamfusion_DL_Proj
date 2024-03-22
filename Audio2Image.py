9899ddf68772604e1689ae2c07451219ca9b37be
Keren Cohen <keren.c91@gmail.com>
2024-03-22 16:14:03 +0200

    initial changes Keren

--- Audio2Image.py
+++ Audio2Image.py
@@ -6,31 +6,22 @@ from diffusers import StableUnCLIPImg2ImgPipeline
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 pipe = StableUnCLIPImg2ImgPipeline.from_pretrained(
     "stabilityai/stable-diffusion-2-1-unclip", torch_dtype=torch.float16
-).to(device)
-model = ib.imagebind_huge(pretrained=True).eval().to(device)
+)
+pipe = pipe.to(device)
 
+model = ib.imagebind_huge(pretrained=True)
+model.eval()
+model.to(device)
 
-
-
-
+# generate image
 with torch.no_grad():
     audio_paths=["audio_files/bird_audio.wav"]
-    embeddings = model.forward({ib.ModalityType.AUDIO: ib.load_and_transform_audio_data(audio_paths, device),})
+    embeddings = model.forward({
+        ib.ModalityType.AUDIO: ib.load_and_transform_audio_data(audio_paths, device),
+    })
+    #embeddings['audio'].shape = torch.Size([1, 1024])
+    #embeddings.keys() = dict_keys(['audio'])
     embeddings = embeddings[ib.ModalityType.AUDIO]
-    images = pipe(prompt='a DSLR photo of a bird front view', image_embeds=embeddings.half()).images
-    images[0].save("audiotext2img.png")
-
-
-
-
-
-
-# # generate image
-# with torch.no_grad():
-#     audio_paths=["assets/wav/bird_audio.wav"]
-#     embeddings = model.forward({
-#         ib.ModalityType.AUDIO: ib.load_and_transform_audio_data(audio_paths, device),
-#     })
-#     embeddings = embeddings[ib.ModalityType.AUDIO]
-#     images = pipe(image_embeds=embeddings.half()).images
-#     images[0].save("audio2img.png")
+    ##embeddings.shape = torch.Size([1, 1024])
+    images = pipe(prompt='a painting', image_embeds=embeddings.half()).images
+    images[0].save("audiotext2img.png")#print(images[0].size) = (768, 768)
